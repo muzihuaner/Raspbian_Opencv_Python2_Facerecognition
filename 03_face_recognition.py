2569e8de653e759a37ed57af00654eb27929b66f
木子欢儿～ <muzihuaner@hotmail.com>
2021-10-18 19:22:21 +0800

    Update 03_face_recognition.py

--- 03_face_recognition.py
+++ 03_face_recognition.py
@@ -23,7 +23,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Lihuan', 'Duan', 'Ilza', 'Z', 'W'] 
+names = ['None','Marcelo', 'Paula', 'Ilza', 'Z', 'W'] 
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
